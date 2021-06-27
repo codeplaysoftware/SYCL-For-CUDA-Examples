d66be3dafadd0995f40a093e77ed44d61fb20adb
Ruyman Reyes <ruyman@codeplay.com>
2021-06-27 15:34:52 +0100

    Using default stream for synchronization

--- example-06/README.md
+++ example-06/README.md
@@ -23,10 +23,14 @@ The SYCL code implemented in the C++ version of the code works as usual with one
 Uses the CUDA Primary context to enable inter-operating with the CUDA Fortran code, ensuring the same resources are shared.
 
 The following snipped highligts the construction of a SYCL context associated with the Primary context.
+To ensure synchronization with the CUDA Fortran code, the queue will also be mapped to the default CUDA
+stream, instead of creating a new stream.
+It is possible to create a normal stream,  just by using the default SYCL queue constructor on the CUDA
+context. Said queue will run concurrently (i.e. won't sync) to the main queue.
 
 ```cpp
 sycl::context c{sycl::property::context::cuda::use_primary_context()};
-sycl::queue q{c, c.get_devices()[0]};
+sycl::queue q{c, c.get_devices()[0], sycl::property::queue::cuda::use_default_stream()};
 ```
 
 
--- example-06/saxpy.cpp
+++ example-06/saxpy.cpp
@@ -2,27 +2,27 @@
 #include <CL/sycl.hpp>
 
 extern "C" {
-	void saxpy_sycl_cuda_wrapper (float* x, float* y, float a, int N);
+   void saxpy_sycl_cuda_wrapper (float* x, float* y, float a, int N);
 };
 
 
 void saxpy_sycl_cuda_wrapper (float* x, float* y, float a, int N) {
-	sycl::context c{sycl::property::context::cuda::use_primary_context()};
-	sycl::queue q{c, c.get_devices()[0]};
-	{
-		sycl::buffer bX {x, sycl::range<1>(N)};
-		sycl::buffer bY {y, sycl::range<1>(N)};
+   sycl::context c{sycl::property::context::cuda::use_primary_context()};
+   sycl::queue q{c, c.get_devices()[0], sycl::property::queue::cuda::use_default_stream()};
+   {
+      sycl::buffer bX {x, sycl::range<1>(N)};
+      sycl::buffer bY {y, sycl::range<1>(N)};
 
-		q.submit([&](sycl::handler& h) {
-			auto aX = bX.get_access<sycl::access::mode::read_write>(h);
-			auto aY = bY.get_access<sycl::access::mode::read_write>(h);
-			h.parallel_for<class saxpy_kernel>(sycl::range<1>(N), [=](sycl::id<1> id) {
-				if (id[0] < N) 
-					aY[id] = aX[id] * a + aY[id];
-			});
-		});
+      q.submit([&](sycl::handler& h) {
+         auto aX = bX.get_access<sycl::access::mode::read_write>(h);
+         auto aY = bY.get_access<sycl::access::mode::read_write>(h);
+         h.parallel_for<class saxpy_kernel>(sycl::range<1>(N), [=](sycl::id<1> id) {
+            if (id[0] < N) 
+               aY[id] = aX[id] * a + aY[id];
+         });
+      });
 
-		q.wait_and_throw();
-	}
-	return;
+      q.wait_and_throw();
+   }
+   return;
 }
