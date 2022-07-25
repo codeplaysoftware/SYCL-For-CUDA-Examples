380fe4f0f9c13228e15fba70fd111c9db3dd4dfd
Joe Todd <joeatodd@users.noreply.github.com>
2022-07-25 16:08:35 +0100

    Add license text for examples (#25)
    * License notice for distributed-batch-gemm
    
    * License notice for fortran-interface
    
    * License text for hash functions
    
    * License text for Kokkos example
    
    * License text for MPI example
    
    * License text for SGEMM
    
    * Use licensed version of vector_addition.cu
    
    * License text for CUDA interop examples

--- examples/hashing/src/hash_functions/sha256.cpp
+++ examples/hashing/src/hash_functions/sha256.cpp
@@ -1,3 +1,26 @@
+/***************************************************************************
+ *
+ *  Copyright (C) Codeplay Software Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Codeplay's SYCL-For-CUDA-Examples
+ *
+ *  sha256.cpp
+ *
+ *  Description:
+ *    SHA256 hash function
+ **************************************************************************/
 #include <hash_functions/sha256.hpp>
 #include <internal/determine_kernel_config.hpp>
 
