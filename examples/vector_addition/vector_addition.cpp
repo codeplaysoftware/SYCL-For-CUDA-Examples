0c886cb3a3281da186100f41155a660b6943931a
Ruyman Reyes Castro <ruyman@codeplay.com>
2023-02-09 17:16:34 +0000

    Vector Addition example update
    Works with latest DPC++ and SYCL2020 features
    README updated to reflect CUDA backend has USM support

--- examples/vector_addition/vector_addition.cpp
+++ examples/vector_addition/vector_addition.cpp
@@ -34,10 +34,12 @@ int main(int argc, char *argv[]) {
 
   // Initialize input data
   {
-    const auto dwrite_t = sycl::access::mode::discard_write;
+    const auto dwrite_t = sycl::write_only;
 
-    auto h_a = bufA.get_access<dwrite_t>();
-    auto h_b = bufB.get_access<dwrite_t>();
+
+    sycl::host_accessor h_a{bufA, sycl::write_only};
+    sycl::host_accessor h_b{bufB, sycl::write_only};
+
     for (int i = 0; i < N; i++) {
       h_a[i] = sin(i) * sin(i);
       h_b[i] = cos(i) * cos(i);
@@ -70,8 +72,8 @@ int main(int argc, char *argv[]) {
   myQueue.submit(cg);
 
   {
-    const auto read_t = sycl::access::mode::read;
-    auto h_c = bufC.get_access<read_t>();
+    sycl::host_accessor h_c{bufC, sycl::read_only};
+
     double sum = 0.0f;
     for (int i = 0; i < N; i++) {
       sum += h_c[i];
